114354a27be11c0ffdfda8e38ebcd19987112e77
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-11 11:23:48 +0200

    Challenge completed

--- js/index.js
+++ js/index.js
@@ -2,21 +2,32 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorDisplay = document.querySelector("p.error");
 
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+      }
+      return a / b;
+    } catch (error) {
+      errorDisplay.textContent = error.message;
+      //To display a additional error message otherwise out would be undefined at every error
+      return "Error! 🤯⛔️♾️";
     }
-    return a / b;
   },
 };
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  //To unset the error message if it got set before
+  errorDisplay.textContent = "";
+
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
